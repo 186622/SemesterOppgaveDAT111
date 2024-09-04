c61dff1413374ad2a036adf733b2481b812019e2
HanssenDevs <66432612+HanssenDevs@users.noreply.github.com>
2024-09-04 10:03:47 +0200

    light mode knapp - style

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
 
-document.getElementById('dark-mode-btn').addEventListener('click', function() {
+document.getElementById('dark-mode-toggle').addEventListener('click', function() {
     document.body.classList.toggle('light-mode');
 });
